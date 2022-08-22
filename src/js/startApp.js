87c775f0dc7d7f82a44d702ded1e463101926a40
bluehawhy <bluehawhy@gmail.com>
2022-08-22 22:46:46 +0900

    change startApp

--- src/js/startApp.js
+++ src/js/startApp.js
@@ -1,4 +1,4 @@
 function show () {document.querySelector(".background").className = "background show";}
 function close () {document.querySelector(".background").className = "background";}
 document.querySelector("#show").addEventListener('click', show);
-document.querySelector("#close").addEventListener('click', close);
+document.querySelector("#close_button").addEventListener('click', close);
